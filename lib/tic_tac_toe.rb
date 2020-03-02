48debdd1c0b19e723fdec9dd8b4ca0a8f46e6001
Richard Cho <96richardohc@gmail.com>
2020-03-02 16:39:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,7 @@
 class TicTacToe
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
   @board=[]
+  
   def initialize
     @board = Array.new(9, " ")
   end
@@ -94,8 +95,6 @@ class TicTacToe
       WIN_COMBINATIONS.detect {|combo| combo.all?{|place| @board[place] =="X"}} ? "X" : "O"
     end
   end
-  
-  
 
   def play
     while !over?
